594537138c8ead5e8a27a9a6cf5821dd7370a18d
Marcelo Tosatti <mtosatti@redhat.com>
2020-06-03 17:04:04 -0300

    print additional information
    Print maximum latency - bias and minimum latency
    for pre-heat stage.
    
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- main.c
+++ main.c
@@ -427,6 +427,8 @@ static void write_summary(struct thread* t)
     putfield("Min Latency", t[i].minlat, PRIu64, " (us)");
     putfield("Average", t[i].average, ".3lf", " (us)");
     putfield("Max Latency", t[i].maxlat, PRIu64, " (us)");
+    if (g.bias)
+	    putfield("MaxLat-Bias", t[i].maxlat - g.bias, PRIu64, " (us)");
     putfield("Duration", cycles_to_sec(&(t[i]), t[i].runtime),
              ".3f", " (sec)");
     printf("\n");
@@ -721,6 +723,7 @@ static void record_bias(struct thread *t)
         }
     }
     g.bias = bias;
+    printf("Global bias set to %" PRId64 " (us)\n", bias);
 }
 
 int main(int argc, char* argv[])
